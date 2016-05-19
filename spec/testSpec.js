bb818bf7528072ea6ae40322e9eb0a3ff37b4eb9
Dan Moore <dan.moore@sykescottages.co.uk>
2016-05-19 17:47:10 +0100

    Added test framework

---
+++ spec/testSpec.js
@@ -0,0 +1,6 @@
+describe('Filter', function() {
+    it('readableEvent should return a readable label for the given event', function() {
+        var result = 'Inactive';
+        expect(result).toEqual('Inactive');
+    });
+});
